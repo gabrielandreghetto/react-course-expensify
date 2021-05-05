cffedbc5d4e9ba9cb3f648d960841c502a21f8af
Gabriel Andreghetto <gabrielandreghetto@gmail.com>
2021-05-05 03:01:35 -0300

    Remove dummy expenses

--- src/app.js
+++ src/app.js
@@ -19,20 +19,6 @@ const history = createBrowserHistory();
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 50 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 10, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1095 }));
-
-// store.dispatch(setTextFilter('bill'));
-
-// setTimeout(() => {
-//     store.dispatch(setTextFilter('water'));
-// }, 3000)
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Router history={history}>
         <Provider store={store}>
