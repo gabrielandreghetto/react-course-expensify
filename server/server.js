8c96689513144fec898b6b8d06045851bd6b49f3
Gabriel Andreghetto <gabrielandreghetto@gmail.com>
2021-05-08 02:54:01 -0300

    Setup prod build and server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path    = require('path');
 const express = require('express');
 
+const port       = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
 
 const app = express();
@@ -11,6 +12,6 @@ app.get('*', (req, res, next) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('connected')
 });
